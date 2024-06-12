ebaa3032cd1b7eda9ab1aaca58e96d304418a0b6
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2024-06-12 21:07:05 +0200

    essentials

---
+++ src/components/taskbar.jsx
@@ -0,0 +1,24 @@
+import '../styling/taskbar.scss';
+
+function taskbar() {
+    return (
+      <div className="taskbar">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2">
+              <div className="first-title">
+                <p>jskdbfhsbfjks</p>
+              </div>
+            </div>
+            <div className="col-md-5"></div>
+            <div className="col-md-5">
+              <p>another test bahah</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default taskbar
+  
