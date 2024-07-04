4ffba1d0f1c6fc59d0f111a7adc44ba234953514
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2024-07-04 14:39:17 +0200

    added typewriter for function and added react slick slider

--- src/components/frontpage.jsx
+++ src/components/frontpage.jsx
@@ -1,6 +1,20 @@
+import React, { useEffect, useRef } from 'react';
+import Typewriter from 'typewriter-effect/dist/core';
 import '../styling/frontpage.scss';
 
 function frontpage() {
+    const typewriterRef = useRef(null);
+
+    useEffect(() => {
+        if (typewriterRef.current) {
+            new Typewriter(typewriterRef.current, {
+                strings: ['Full Stack Developer', 'MERN-Stack Developer'],
+                autoStart: true,
+                loop: true,
+            });
+        }
+    }, []);
+
     return (
         <div className="frontpage">
             <div className="row">
@@ -8,7 +22,9 @@ function frontpage() {
                     <div className="textbox">
                         <div className="text">Hi there!</div>
                         <div className="name">I'm <span style={{ color: '#377747' }}>Gene Zijlmans</span></div>
-                        <div className="function">Full Stack developer</div>
+                        <div className="function">
+                            <span ref={typewriterRef}></span>
+                        </div>
                     </div>
                 </div>
                 <div className="col-md-4">
--- src/components/techstack.jsx
+++ src/components/techstack.jsx
@@ -1,11 +1,40 @@
 import '../styling/techstack.scss';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function techstack() {
+    const settings = {
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    };
+
     return (
       <div className="techstack">
         <div className="title-wrapper">
             <div className="number">01.</div>
             <div className="title">techstack</div>
+        </div>
+        <div className='carousel'>
+            <h2>Carousel Component</h2>
+            <Slider {...settings}>
+                <div>
+                <h3>FIRST SLIDE</h3>
+                </div>
+                <div>
+                <h3>SECOND SLIDE</h3>
+                </div>
+                <div>
+                <h3>THIRD SLIDE</h3>
+                </div>
+                <div>
+                <h3>FOURTH SLIDE</h3>
+                </div>
+            </Slider>
         </div>
       </div>
     )
