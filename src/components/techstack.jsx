1459d473edbab6864bc7c7b90d1080bd499a683a
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2024-07-06 13:30:29 +0200

    techstack slider completely done

--- src/components/techstack.jsx
+++ src/components/techstack.jsx
@@ -7,8 +7,8 @@ function Techstack() {
   const settings = {
     infinite: true,
     centerMode: true,
-    speed: 5000,
-    slidesToShow: 6,
+    speed: 4000,
+    slidesToShow: 4,
     slidesToScroll: 2,
     autoplay: true,
     autoplaySpeed: 0,
@@ -27,63 +27,109 @@ function Techstack() {
         <div className="title">techstack</div>
       </div>
       <div className="text-box">Skills & <span style={{ color: '#377747' }}>Tools</span></div>
-      <div className="slider-wrapper">
-        <Slider {...settings}>
-          <div className="slide-content">
-            <div className="image-wrapper">
-              <div className="image html"></div>
-            </div>
-            <div className="title-wrapper">HTML</div>
+      <div className="container">
+        <div className="mx-auto">
+          <div className="slider-wrapper">
+            <Slider {...settings}>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                  <div className="image html"></div>
+                </div>
+                <div className="title-wrapper">HTML</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                  <div className="image css"></div>
+                </div>
+                <div className="title-wrapper">CSS</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                  <div className="image javascript"></div>
+                </div>
+                <div className="title-wrapper">Javascript</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                  <div className="image tailwind"></div>
+                </div>
+                <div className="title-wrapper">Tailwind</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image react"></div>
+                </div>
+                <div className="title-wrapper">React</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image nodejs"></div>
+                </div>
+                <div className="title-wrapper">NodeJs</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image mongodb"></div>
+                </div>
+                <div className="title-wrapper">MongoDB</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image mongoose"></div>
+                </div>
+                <div className="title-wrapper">Mongoose</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image express"></div>
+                </div>
+                <div className="title-wrapper">Express</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image Webpack"></div>
+                </div>
+                <div className="title-wrapper">Wepack</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image npm"></div>
+                </div>
+                <div className="title-wrapper">Npm</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image git"></div>
+                </div>
+                <div className="title-wrapper">Git</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image vscode"></div>
+                </div>
+                <div className="title-wrapper">VsCode</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image vim"></div>
+                </div>
+                <div className="title-wrapper">Vim</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image terminal"></div>
+                </div>
+                <div className="title-wrapper">Terminal</div>
+              </div>
+              <div className="slide-content">
+                <div className="image-wrapper">
+                <div className="image linux"></div>
+                </div>
+                <div className="title-wrapper">Linux</div>
+              </div>
+            </Slider>
           </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-              <div className="image css"></div>
-            </div>
-            <div className="title-wrapper">CSS</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-              <div className="image javascript"></div>
-            </div>
-            <div className="title-wrapper">Javascript</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-              <div className="image tailwind"></div>
-            </div>
-            <div className="title-wrapper">Tailwind</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-            <div className="image react"></div>
-            </div>
-            <div className="title-wrapper">React</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-            <div className="image nodejs"></div>
-            </div>
-            <div className="title-wrapper">NodeJs</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-            <div className="image mongodb"></div>
-            </div>
-            <div className="title-wrapper">MongoDB</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-            <div className="image mongoose"></div>
-            </div>
-            <div className="title-wrapper">Mongoose</div>
-          </div>
-          <div className="slide-content">
-            <div className="image-wrapper">
-            <div className="image express"></div>
-            </div>
-            <div className="title-wrapper">Express</div>
-          </div>
-        </Slider>
+        </div>
       </div>
     </div>
   );
